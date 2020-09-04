396329983d0c0dfb2abe108af5d66936da4ef5ed
Dave <david.quinta415@gmail.com>
2020-09-04 10:28:23 -0600

    Menu SB

--- functions/index.js
+++ functions/index.js
@@ -298,32 +298,14 @@ async function formateResponse(id, user_txt, sentiment, message, ansimg) {
     1-> Woman
   */
 
-  switch (ansimg) {
-    case 0:
-      img =
-        "https://firebasestorage.googleapis.com/v0/b/castlefortress-game.appspot.com/o/characters%2Fdude0.jpeg?alt=media&token=d637efa8-866d-4acc-b141-73b07d7bf119";
-      break;
-    case 1:
-      img =
-        "https://firebasestorage.googleapis.com/v0/b/castlefortress-game.appspot.com/o/characters%2Fdude1.jpeg?alt=media&token=1c22ec2e-23bd-4e56-9229-e19fd08d28f8";
-      break;
-    case 7:
-      img =
-        "https://firebasestorage.googleapis.com/v0/b/castlefortress-game.appspot.com/o/istockphoto-984735510-640x640.jpg?alt=media&token=b6d21833-4b76-4b99-912f-373b392aea28";
-      break;
-
-    default:
-      img = "";
-      break;
-  }
-
   let data = {
     idChat: id,
     user_txt: user_txt,
     uid: "X4TkYxTgloQlFjgPKO6ciKSUeL63",
     sentiment: sentiment,
     message: message,
-    img_url: img,
+    img_avatar: 1,
+    img_landscape: 1,
     speech: speech,
   };
 
@@ -331,114 +313,55 @@ async function formateResponse(id, user_txt, sentiment, message, ansimg) {
 }
 
 //Setup TEST Wit.AI
-app.post("/witai_test", (req, res) => {
-  const message = req.body.message;
-  console.log(message);
-  client
-    .message(message)
+
+//Ending Test API
+
+//THIS IS THE REAL DEAL----
+app.post("/gameNPL", (req, res) => {
+  console.info("DATA_RECIVED", req.body);
+
+  const url = "https://api.wit.ai/speech";
+  const fileCreated = arrayBufferToBase64(req.body.audioContent.data);
+  //console.log("FILE CREATED AFTER RESPONSE", fileCreated);
+  const buffered_response = Buffer.from(fileCreated, "base64");
+  let rdata = {
+    id: req.body.idChat,
+    audioContent: buffered_response,
+  };
+
+  console.log("OBJETO", rdata);
+  // res.status(200).send(rdata);
+  return fetch(url, {
+    method: "POST",
+    body: buffered_response,
+    headers: {
+      "Content-Type": "audio/wav",
+      Authorization: "Bearer WJNHREJLFURRUENIER2I5W5OVBWUGTWP",
+    },
+  })
+    .then((res) => res.json())
     .then(async (data) => {
+      //Here is where the magic happens
       const intent = (data.intents.length > 0 && data.intents[0]) || "__foo__";
+      const user_txt = data.text;
+
+      const entities =
+        (data.entities.length > 0 && data.entities[0]) || "__foo__";
 
       let ans = "";
       switch (intent.name) {
-        case "distanceBetween":
-          ans = await handleDistanceBetween(data);
-          console.log(ans);
-          res.status(200).send(ans);
-          break;
-        case "timeAtPlace":
-          ans = await handleTimeAtPlace(data);
-          console.log(ans);
-          res.status(200).send(ans);
-          break;
+        case "greetingIntent":
+          ans = await handleGreeting(data);
+          //await docRef.add(ans);
+          return res.status(200).send(ans);
+        case "insultIntent":
+          ans = await handleInsult(data);
+          //await docRef.add(ans);
+          return res.status(200).send(ans);
       }
-
-      res.status(202).send("Sorry Bruh, I don't know what to answer you :'v");
+      ans = await handleNone(data);
+      //await docRef.add(ans);
+      return res.status(200).send(ans);
     })
-    .catch((error) => res.status(400).send(error));
+    .catch((error) => res.status(500).send(error));
 });
-
-function handleGibberish() {
-  return res
-    .status(202)
-    .send("Sorry Bruh, I don't know what to answer you :'v");
-}
-
-// ----------------------------------------------------------------------------
-// handleDistanceBetween
-
-function handleDistanceBetween(data) {
-  const location = data.entities["wit$location:location"];
-  if (location == null || location.length != 2) {
-    return handleGibberish();
-  }
-
-  var loc0 = location[0].resolved.values[0];
-  var loc1 = location[1].resolved.values[0];
-  var distance = getDistanceFromLatLonInKm(
-    loc0.coords.lat,
-    loc0.coords.long,
-    loc1.coords.lat,
-    loc1.coords.long
-  );
-  distance = roundTo(distance, 0.01);
-  return Promise.resolve(
-    `It's ${distance}km from ${loc0.name} to ${loc1.name}`
-  );
-}
-
-//https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2 - lat1); // deg2rad below
-  var dLon = deg2rad(lon2 - lon1);
-  var a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
-}
-
-function roundTo(val, round) {
-  return Math.floor(val / round) * round;
-}
-
-// ----------------------------------------------------------------------------
-// handleTimeAtPlace
-
-function handleTimeAtPlace(data) {
-  const loc =
-    data.entities["wit$location:location"] &&
-    data.entities["wit$location:location"][0];
-  if (loc == null) {
-    return handleGibberish();
-  }
-
-  const tz = loc.resolved.values[0].timezone;
-  const placeName = loc.resolved.values[0].name;
-
-  return currentTimeFromTimezone(tz).then((res) => {
-    return `It's currently ${res} in ${placeName}`;
-  });
-}
-
-function currentTimeFromTimezone(loc) {
-  const url = "http://worldtimeapi.org/api/timezone/" + loc;
-
-  return fetch(url, {})
-    .then((res) => res.json())
-    .then((data) => {
-      //trim off the timezone to avoid date auto-adjusting
-      const time = data.datetime.substring(0, 19);
-      return new Date(time).toUTCString("en-US").substring(0, 22);
-    });
-}
-//Ending Test API
